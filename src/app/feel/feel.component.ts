79cad86d3c7adae3107011d2812da214b1270dc0
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2024-04-23 18:43:58 +0200

    feel component done

--- src/app/feel/feel.component.ts
+++ src/app/feel/feel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-feel',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './feel.component.html',
   styleUrl: './feel.component.scss'
 })
